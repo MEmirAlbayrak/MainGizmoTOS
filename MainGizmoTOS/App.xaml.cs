48355cc2939d89d6083d11c625442e688f00efcc
Stream <Stream@DESKTOP-KTSA74B>
2021-09-17 18:08:05 +0300

    Proje dosyası ekle.

---
+++ MainGizmoTOS/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Client;
+using SharedLib;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MainGizmoTOS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            // Create the window
+            MainWindow window = new MainWindow();
+
+            window.Show();
+
+        }
+
+
+
+
+    }
+
+
+
+}
---
+++ MainGizmoTOS/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Client;
+using SharedLib;
+
+namespace MainGizmoTOS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window 
+    {
+
+        public bool testb = false;
+
+        private void OnLoginStateChange(object sender, UserEventArgs e)
+        {
+
+
+            if (e.State.HasFlag(LoginState.LoginCompleted))
+            {
+
+                if(e.UserProfile.PostCode != null)
+                {
+                    CloseGUI();
+
+                }
+                else
+                {
+                    Test(sender, e);
+                    e.UserProfile.PostCode = "yes";
+                }
+
+            }
+
+
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        public void Test(object sender, UserEventArgs e)
+        {
+
+            while (true)
+            {
+                if (testb)
+                {
+                    DateTime dateTime = DateTime.UtcNow.Date;
+                    e.UserProfile.PostCode = dateTime.ToString("ddMMyyyy");
+
+                    testb = false;
+                    CloseGUI();
+
+                }
+
+            }
+
+
+        }
+
+        private void Click_Yes(object sender, RoutedEventArgs e)
+        {
+
+            //import Date to user
+            WriteDate();
+
+        }
+
+        private void Click_No(object sender, RoutedEventArgs e)
+        {
+            CloseGUI();
+        }
+
+        public void WriteDate() {
+
+            //Write Date to User    
+            testb = true;
+        
+        }
+
+        public void CloseGUI()
+        {
+            this.Close();
+        }
+
+  
+
+    }
+}
+
+
+
